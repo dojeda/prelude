bddcdd19ebc78337272b7c91b85dc1516fcb22b5
David Ojeda <david.ojeda@gmail.com>
2014-02-14 00:21:45 +0100

    some packages were missing: very weird, specially projectile

--- personal/00-packages.el
+++ personal/00-packages.el
@@ -1,2 +1,2 @@
 ;; This are the packages that I want automatically installed 
-(prelude-require-packages '(auctex auctex-latexmk nlinum cmake-mode auto-complete auto-complete-clang magit project-explorer cmake-mode jedi window-number smart-mode-line))
+(prelude-require-packages '(auctex auctex-latexmk nlinum cmake-mode auto-complete auto-complete-clang magit project-explorer cmake-mode jedi window-number smart-mode-line multi-term yasnippet))
